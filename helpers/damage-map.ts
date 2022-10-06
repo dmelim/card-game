74fcaa32b1968c36e92c96f4cd334a7598a96080
dmelim <dmpmelim@gmail.com>
2022-10-06 19:21:48 +0100

    Created damage map.

---
+++ helpers/damage-map.ts
@@ -0,0 +1,7 @@
+export const ELEMENTS_MAP = {
+  ["fire"]: { weakness: "water", strength: "grass" },
+  ["water"]: { weakness: "electric", strength: "fire" },
+  ["electric"]: { weakness: "ground", strength: "water" },
+  ["grass"]: { weakness: "fire", strength: "ground" },
+  ["ground"]: { weakness: "grass", strength: "electric" },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
-import type { NextPage } from "next";
-
-const Home: NextPage = () => {
+import type { GetStaticProps } from "next";
+import { ELEMENTS_MAP } from "../helpers/damage-map";
+type Props = {
+  fire: { weakness: string; strength: string };
+};
+const Home = ({ fire }: Props) => {
   return (
     <div>
-      <h1>Welcome to the Game</h1>
+      <h1>{fire.weakness}</h1>
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const fire = ELEMENTS_MAP.fire;
+  return {
+    props: { element: fire },
+  };
 };
 
 export default Home;
